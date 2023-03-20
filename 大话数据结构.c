0461c6674ce8d2ae338891bc887e3a66de1d484d
Tuki010 <50453882+Tuki010@users.noreply.github.com>
2023-03-20 15:49:34 +0800

    Update 大话数据结构.c

--- 大话数据结构.c
+++ 大话数据结构.c
@@ -213,6 +213,14 @@
   线性表的顺序存储结构
     定义：指的是用一段地址连续的存储单元依次存储线性表的数据元素。
     顺序存储方式：
+      顺序存储结构代码：
+        #define MAXSIZE 20    /*存储空间初始分配量*/
+        typedef int ElemType; /*ElemType 类型根据实际情况而定，这里假设为int*/
+        typedef struct
+        {
+          ElemType data[MAXSIZE];  /*数组存储数据元素，最大值为MAXSIZE */
+          int length;              /*线性表当前长度*/
+        }sqList;
       
 第四章 栈与队列
 第五章 串
